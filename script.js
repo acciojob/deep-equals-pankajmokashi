62b6d91c6c17a3c9d06bdb9eb1ace24702127ed0
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-05-16 18:15:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function deepEquals(a, b) {
- 
+function objectEquals(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
+function deepEquals(a, b) {
+    if(a === null && b === null){
+        return true
+    }
+    if(typeof(a) !== typeof(b))
+    {
+        return false
+    }
+    else if(typeof(a) === "object"){
+        if(objectEquals(a, b) === true){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+	if(a === b){
+		 return true
+	}
+	return false
+}
 module.exports=deepEquals;
